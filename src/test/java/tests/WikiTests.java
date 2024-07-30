40614b419e42ac73e92cdecbfdf8c489ee0417c0
nastasj <a-nastja-test1@yandex.ru>
2024-07-30 20:05:54 +0300

    add mobile tests

--- src/test/java/tests/WikiTests.java
+++ src/test/java/tests/WikiTests.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
-import static io.appium.java_client.AppiumBy.id;
+import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
 
 public class WikiTests extends TestBase {
@@ -22,9 +22,7 @@ public class WikiTests extends TestBase {
             $(accessibilityId("Search Wikipedia")).click();
             $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         });
-        step("Verify content found", () ->
-                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
+        step("Verify content found", () -> $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0)));
     }
 
     @Test
@@ -38,6 +36,18 @@ public class WikiTests extends TestBase {
             $(id("com.google.android.tts:id/transcription_mic_image_indicator")).shouldBe(visible);
             $(id("com.google.android.tts:id/transcription_intent_api_text")).shouldHave(text("Try saying something"));
         });
+    }
+
+    @Test
+    @Tag("android")
+    @DisplayName("Check open article")
+    void successfulOpenArticleTest() {
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys(" QA");
+        });
+        step("Open article", () -> $(id("org.wikipedia.alpha:id/page_list_item_title")).click());
+        step("Check article", () -> $(xpath("//android.widget.TextView[@text='QA']")).shouldBe(visible));
     }
 }
 
